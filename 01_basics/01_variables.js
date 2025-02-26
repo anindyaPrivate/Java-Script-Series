948f5485706f011d221593b7a1e1fccc41c2066e
Anindya Das <87466374+anindyaPrivate@users.noreply.github.com>
2025-02-26 07:02:44 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345 // cant change the value 
+let accountEmail = "anindya@gmail.com"
+var accountPassword = "23145" /* prefer not to use var issue in block scope and fuctional scope */
+accountCity = "Kolkata"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
