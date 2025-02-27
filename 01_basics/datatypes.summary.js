b36af3f86264e0a30662b119ae8aeef2e16595b9
Anindya Das <87466374+anindyaPrivate@users.noreply.github.com>
2025-02-27 06:50:03 +0000

    Memory(stack and heap)

--- 01_basics/datatypes.summary.js
+++ 01_basics/datatypes.summary.js
@@ -36,3 +36,34 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ====================================================================================================
+
+/************* MEMORY **************/
+
+// Stack = primitive (get copy)     
+// Heap = Non Premitive(change in original)
+
+
+let myName = "Anindya"
+
+let myAnotherName = myName
+myAnotherName = "Jill"
+// console.log(myName);
+// console.log(myAnotherName);
+
+// heap
+
+let userOne = {
+    email: "abc@mail.com",
+    bank :"Sbi"
+}
+
+let userTwo = userOne
+
+userTwo.bank = "Hdfc"
+console.log(userOne);
+console.log(userTwo);
+
+
