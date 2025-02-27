65a29359364df8d85a422897b56cf85574e027bc
Anindya Das <87466374+anindyaPrivate@users.noreply.github.com>
2025-02-27 05:31:13 +0000

    Operations

--- 01_basics/03_conversionOpearation.js
+++ 01_basics/03_conversionOpearation.js
@@ -42,5 +42,50 @@ let booleanIsloggedin = Boolean(isLoggedIn)
 
 let somenumber = "45"
 let stringNumber = String(somenumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+//********  Operations ********//
+
+let value = 5
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Anindya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5 % 3);
+
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
